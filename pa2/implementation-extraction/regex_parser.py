b2e2d995a9c5f2c4ef548a440755a210d774f079
KristjanPanjan <kristjan.panjan@gmail.com>
2021-04-27 14:37:26 +0200

    Fixes and polishing

--- pa2/implementation-extraction/regex_parser.py
+++ pa2/implementation-extraction/regex_parser.py
@@ -23,56 +23,62 @@ cleaner = Cleaner()
 def regex_parser(html_code, page_source):
 
     for page in rtv_slo:
-        tmp = read_page(page)
+        tmp = prepare_page(page)
         tmp2 = parse_rtv(tmp)
 
 
     for page in over_stock:
-        tmp = read_page(page)
+        tmp = prepare_page(page)
         tmp2 = parse_overstock(tmp)
 
 
     for page in siol:
-        tmp = read_page(page)
+        tmp = prepare_page(page)
         tmp2 = parse_siol(tmp)
+
+        #print(tmp2)
 
     return 0
 
 
-def read_page(page_path):
-    return codecs.open(page_path, encoding='utf-8', errors='replace').read()
+def prepare_page(page_path):
+    page = codecs.open(page_path, encoding='utf-8', errors='replace').read()
+    return cleaner.clean_html(page)
 
 
 def parse_rtv(page):
 
-    page = cleaner.clean_html(page)
-
+    #Author - RTV
     regex = r"<div class=\"author-name\">(.*)</div>"
     match = re.compile(regex).search(page)
     author = match.group(1)
 
+    #Title - RTV
+    regex = r"<h1>(.*)<\/h1>"
+    match = re.compile(regex).search(page)
+    title = match.group(1)
+
+    #Subtitle - RTV
+    regex = r"<div class=\"subtitle\">(.*)</div>"
+    match = re.compile(regex).search(page)
+    subtitle = match.group(1)
+
+    #Date - RTV
     regex = r"<div class=\"publish-meta\">(.*?)<br>"
     match = re.search(regex, page, re.DOTALL)
     date = match.group(1)
     date = ' '.join(date.split())
 
-    regex = r"<h1>(.*)<\/h1>"
-    match = re.compile(regex).search(page)
-    title = match.group(1)
-
-    regex = r"<div class=\"subtitle\">(.*)</div>"
-    match = re.compile(regex).search(page)
-    subtitle = match.group(1)
-
     regex = r"<p class=\"lead\">(.*)</p>"
     match = re.compile(regex).search(page)
     abstract = match.group(1)
 
+    #Content - RTV
     regex = r"<article class=\"article\">(.*?)<\/article>"
     match = re.search(regex, page, re.DOTALL)
-    content = match.group(1)
-    content2 = ' '.join(content.split())
-    content3 = re.sub(r"<[^>]*>", "", content2)
+    first = ' '.join(match.group(1).split())
+    content = re.sub(r"<[^>]*>", "", first)
+    #print(content)
 
     return (
         json.dumps(
@@ -82,118 +88,111 @@ def parse_rtv(page):
                 "Title": title,
                 "SubTitle": subtitle,
                 "Abstract": abstract,
-                "Content": content3
+                "Content": content
             },
             ensure_ascii=False)
     )
 
 def parse_overstock(page):
-    productId = 0
-    products = []
+    prodId = 0
+    prdcts = []
 
+    #Title - Overstock
     regex = r"<b>([0-9]+-(.+)(?=\</b>))</b>"
     titles = []
     matches = re.finditer(regex, page)
     for match in matches:
         titles.append(match.group(1))
 
+    #LastPrices - Overstock
     regex = r"<s>(.*?)</s>"
     lastPrices = []
     matches = re.finditer(regex, page)
     for match in matches:
         lastPrices.append(match.group(1))
 
-
+    #Prices - Overstock
     regex = r"<b>([$€]\s*[0-9\.,]+)</b>"
     prices = []
     matches = re.finditer(regex, page)
     for match in matches:
         prices.append(match.group(1))
 
+    # Content - OverStock
+    regex = r"<td valign=\"top\"><span class=\"normal\">(.*?)<\/b>"
+    contents = []
+    matches = re.finditer(regex, page, re.DOTALL)
+    for match in matches:
+        content = ' '.join(match.group(1).split())
+        final = re.sub(r"<[^>]*>", "", content)
+        contents.append(final)
 
+    #Percents - Overstock
     regex = r"\(([0-9]+%\))"
     percents = []
     matches = re.finditer(regex, page)
     for match in matches:
         percents.append(match.group(0))
 
-
+    #Savings - Overstock
     regex = r"([$€]\s*[0-9\.,]+ )"
     savings = []
     matches = re.finditer(regex, page)
     for match in matches:
         savings.append(match.group(1).strip())
 
-
-    regex = r"<td valign=\"top\"><span class=\"normal\">(.*?)<\/b>"
-    descriptions = []
-    matches = re.finditer(regex, page, re.DOTALL)
-    for match in matches:
-        con = match.group(1)
-        con2 = ' '.join(con.split())
-        con3 = re.sub(r"<[^>]*>", "", con2)
-        descriptions.append(con3)
-
-    dolzina = len(titles)
-
-    while dolzina >= productId:
-        # print(titles[0])
+    while len(titles) >= prodId:
         try:
-            title = titles[productId]
-            lastPrice = lastPrices[productId]
-            price = prices[productId]
-            saving = savings[productId]
-            savingPercent = percents[productId]
-            description = descriptions[productId]
-            products.append(
+            prdcts.append(
                 {
-                    "ListPrice": lastPrice,
-                    "Price": price,
-                    "Saving": saving,
-                    "SavingPercent": savingPercent,
-                    "Title": title,
-                    "Content": description
-                }
+                    "ListPrice": lastPrices[prodId],
+                    "Price": prices[prodId],
+                    "Saving": savings[prodId],
+                    "SavingPercent": percents[prodId],
+                    "Title": titles[prodId],
+                    "Content": contents[prodId]
+                },
             )
         except:
             pass
 
-        productId = productId + 1
+        prodId = prodId + 1
 
-    return json.dumps([ob for ob in products])
+    return json.dumps([dump for dump in prdcts])
+
 
 def parse_siol(page):
-    page = cleaner.clean_html(page)
 
+    #Title - Siol
     regex = r"<\s*h1[^>]*>(.*?)<\s*/\s*h1>"
     match = re.compile(regex).search(page)
     title = match.group(1)
 
-
+    #Author - Siol
     regex = r"<a href=\"/avtorji/(.*)>(.*)"
     match = re.compile(regex).search(page)
-    con = match.group(2)
     author = re.sub(r'[^. š A-Za-z0-9]+', '', match.group(2))
 
-
+    #Date - Siol
     regex = r"<span class=\"article__publish_date--date\">(.*?)</span>"
     match = re.search(regex, page, re.DOTALL)
     con = match.group(1).replace("\n", "").replace(";", "")
     date = re.sub(r'[^.A-Za-z0-9]+', '', con)
 
-
+    #First paragraph - Siol
     regex = r"<div class=\"article__intro js_articleIntro\">(.*?)</div>"
     match = re.search(regex, page, re.DOTALL)
     first = match.group(1)
     content3 = re.sub(r"<[^>]*>", "", first)
-    abstract = re.sub(r'[^. šŠčČžŽA-Za-z0-9]+', '', content3)
+    abstract = re.sub(r'[^. šŠčČžŽA-Za-z0-9]+', "", content3)
 
-
+    #Content - Siol
     regex = r"<div class=\"article__main js_article js_bannerInArticleWrap\">(.*?)</div>"
     match = re.search(regex, page, re.DOTALL)
     first = match.group(1)
     con = re.sub(r"<[^>]*>", "", first)
-    content = re.sub(r'[^. šŠčČžŽA-Za-z0-9]+', '', con)
+    content = re.sub(r'[^. šŠčČžŽA-Za-z0-9]+', "", con)
+    print(content)
 
     return (
         json.dumps(
